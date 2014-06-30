2cb6bce4eb9c7e37744c3b3d147417a445730e14
Phillip Lanclos <flipxfx@gmail.com>
2014-06-30 10:33:59 -0500

    Use plain javascript, remove jQuery

--- trelloBookmark.js
+++ trelloBookmark.js
@@ -14,96 +14,85 @@ function trelloBookmark(listId) {
   var head = document.getElementsByTagName("head")[0];
   var script = document.createElement("script");
   script.type = "text/javascript";
-  script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js";
+  script.src = "https://api.trello.com/1/client.js?key=a06d07618dfbbdb4c0e94b45041c26f3";
   head.appendChild(script);
-  var script2 = document.createElement("script");
-  script2.type = "text/javascript";
-  script2.src = "https://api.trello.com/1/client.js?key=a06d07618dfbbdb4c0e94b45041c26f3";
-  head.appendChild(script2);
 
-  //Wait to continue until jQuery is loaded
-  var interval = self.setInterval(function(){
-    if(jQuery) {
-      window.clearInterval(interval);
-
-      //Determines the name and description of the card then calls checkAuth
-      function populateCard() {
-        //SG Work Request
-        if ($("#00N400000023eDQ_ileinner") != null && $("#00N400000023eDQ_ileinner").text().trim() != "") {
-          name = $("#Name_ileinner").text() + " - " + $("#00N400000023eDQ_ileinner").text();
-          desc = document.URL.substring(0, 48);
-        }
-        //SG PPM Project
-        else if ($("#00N400000024KiK_ileinner") != null && $("#00N400000024KiK_ileinner").text().trim() != "") {
-          name = $("#00N400000024KiK_ileinner").text() + " - " + $("#Name_ileinner").text();
-          desc = document.URL.substring(0, 48);
-        }
-        //SG Case
-        else if ($(".cas14j_id0_j_id4_ileinner") != null && $("#cas14j_id0_j_id4_ileinner").text().trim() != "") {
-          name = "CASE-" + $(".pageDescription").text().trim() + " - " + $("#cas14j_id0_j_id4_ileinner").text();
-          desc = document.URL.substring(0, 92);
-        }
-        //User is not on a specifically supported page
-        else {
-          if (allowAllPages) {
-            name = document.title;
-            desc = document.URL;
-          }
-          else {
-            alert("Page not supported.");
-            return;
-          }
-        }
-
-        checkAuth();
-      }
-
-      //Try to authorize without user
-      function checkAuth() {
-        Trello.authorize({
-          interactive: false,
-          persist: true,
-          error: function() { auth(); },
-          success: function() { sendToTrello(); }
-        });
-      }
-
-      //If automatic authorize fails then authorize with user
-      function auth() {
-        Trello.authorize({
-          type: "popup",
-          name: "Trello Bookmark",
-          persist: true,
-          scope: { write: true, read: true },
-          expiration: "never",
-          success: function() { sendToTrello(); }
-        });
-      }
-
-      //Logs out the user
-      function deauth() {
-        Trello.deauthorize();
-      }
-
-      //Once the user is logged in this function is fired
-      function sendToTrello() {
-        //Confirm the submission
-        if (window.confirm("Are you sure?")) {
-          //Post the card to Trello
-          Trello.post("cards", {
-            name: name,
-            desc: desc,
-            idList: listId
-          }, function(data) {
-          }, function(data) {
-            //If failed, then try a complete reset
-            alert("Error posting card, please refresh the page and try again.");
-            deauth();
-          });
-        }
-      }
-
-      $(populateCard);
+  //Determines the name and description of the card then calls checkAuth
+  function populateCard() {
+    //SG Work Request
+    if (document.getElementById("00N400000023eDQ_ileinner") != null && document.getElementById("00N400000023eDQ_ileinner").text().trim() != "") {
+      name = document.getElementById("Name_ileinner").innerText + " - " + document.getElementById("00N400000023eDQ_ileinner").innerText;
+      desc = document.URL.substring(0, 48);
     }
-  }, 300);
+    //SG PPM Project
+    else if (document.getElementById("00N400000024KiK_ileinner") != null && document.getElementById("00N400000024KiK_ileinner").text().trim() != "") {
+      name = document.getElementById("00N400000024KiK_ileinner").innerText + " - " + document.getElementById("Name_ileinner").innerText;
+      desc = document.URL.substring(0, 48);
+    }
+    //SG Case
+    else if (document.getElementById("cas14j_id0_j_id4_ileinner") != null && document.getElementById("cas14j_id0_j_id4_ileinner").text().trim() != "") {
+      name = "CASE-" + document.getElementsByClassName("pageDescription")[0].innerText.trim() + " - " + document.getElementById("cas14j_id0_j_id4_ileinner").innerText;
+      desc = document.URL.substring(0, 92);
+    }
+    //User is not on a specifically supported page
+    else {
+      if (allowAllPages) {
+        name = document.title;
+        desc = document.URL;
+      }
+      else {
+        alert("Page not supported.");
+        return;
+      }
+    }
+
+    checkAuth();
+  }
+
+  //Try to authorize without user
+  function checkAuth() {
+    Trello.authorize({
+      interactive: false,
+      persist: true,
+      error: function() { auth(); },
+      success: function() { sendToTrello(); }
+    });
+  }
+
+  //If automatic authorize fails then authorize with user
+  function auth() {
+    Trello.authorize({
+      type: "popup",
+      name: "Trello Bookmark",
+      persist: true,
+      scope: { write: true, read: true },
+      expiration: "never",
+      success: function() { sendToTrello(); }
+    });
+  }
+
+  //Logs out the user
+  function deauth() {
+    Trello.deauthorize();
+  }
+
+  //Once the user is logged in this function is fired
+  function sendToTrello() {
+    //Confirm the submission
+    if (window.confirm("Are you sure?")) {
+      //Post the card to Trello
+      Trello.post("cards", {
+        name: name,
+        desc: desc,
+        idList: listId
+      }, function(data) {
+      }, function(data) {
+        //If failed, then try a complete reset
+        alert("Error posting card, please refresh the page and try again.");
+        deauth();
+      });
+    }
+  }
+
+  populateCard();
 }
